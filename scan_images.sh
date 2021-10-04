4fba3206536edaede482766c377c13dcf69989fd
Chris Black <chris.black@lacework.net>
2021-10-04 10:22:15 -0700

    test script

---
+++ scan_images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+i=0
+while [ $i -ne 5 ]
+do
+        i=$(($i+1))
+        echo "$i"
+        execute_scan
+done
+
+execute_scan() {
+./bin/lw-scanner evaluate aedifex_hack latest \
+    -d=/home/circleci/project/ \
+    --scan-library-packages  \
+    --tags ci_provider=circleci,user=aedifex
+}
